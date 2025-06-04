c086b0812bc4be711343961dcffee61e14693729
David Kalmus <dejvios@seznam.cz>
2025-06-04 17:41:30 +0200

    VueJobs

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>Vue Jobs</h1>
+  <h1 class="text-red-700">Vue Jobs</h1>
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
+    },
+  },
+  plugins: [],
+}
